749246a0a6c5fdb69a8adff38226cc3f7e2a2a08
plouis01 <pierrelouishaboury@gmail.com>
2026-03-17 18:56:17 +0100

    feat: SDK test & SDK framework adapters (LangChain, Eliza, GOAT)

---
+++ sdk/packages/langchain/src/index.ts
@@ -0,0 +1,5 @@
+export { MultisubExecuteTool } from "./tools/execute";
+export { MultisubTransferTool } from "./tools/transfer";
+export { MultisubBudgetTool } from "./tools/budget";
+export { createMultisubTools } from "./toolkit";
+export type { MultisubToolConfig } from "./toolkit";
